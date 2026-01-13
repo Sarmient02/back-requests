beef9fe77cbff4f2c9ad1ba44bed261085818b55
Javier Sarmiento <javiersarmiento02@gmail.com>
2026-01-13 13:33:23 -0500

    initialize spring project

---
+++ src/main/java/com/proteccion/requests/BackRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.proteccion.requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRequestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackRequestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proteccion/requests/BackRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proteccion.requests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRequestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
